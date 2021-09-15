ca456e898342bd4468ea4d964acdd4ad7074e360
hwu5542 <hwu5542@gmail.com>
2021-09-15 13:49:08 -0500

    converted game.html

--- simonfrontend/src/components/game.tsx
+++ simonfrontend/src/components/game.tsx
@@ -8,4 +8,30 @@ export const game = (sequenceLength:number) => {
     while (++times < sequenceLength) {
         newSequence += Math.trunc(Math.random()*100) % 4;
     }
+
+    return (<div>
+                <div className="wholegame" style={{padding-top: 25px, padding-bottom: 20px}}>
+                    <div className="circle" >
+                        <div className="container" style={{padding-top: 25px}}>
+                            <div className="row justify-content-center">
+                                <div className="col aligncenter" >
+                                    <img src="https://cdn.discordapp.com/attachments/887715476396978176/887720934939693126/greenDark.png" style={{border: none}} alt="green"/>
+                                </div>
+                                <div className="col">
+                                    <img src= "https://media.discordapp.net/attachments/887715476396978176/887720930284011550/redDark.png" style={{border: none}} alt="red"/>
+                                </div>
+                            </div>
+                            <div className="row justify-content-center" style={{margin-top: 20px}}>
+                                <div className="col aligncenter" style={{margin-left: 0px}}>
+                                    <img  src="https://media.discordapp.net/attachments/887715476396978176/887720937544376390/yellowDark.png"style={{border: none}} alt="yello"/>
+                                </div>
+                                <div className="col">
+                                    <img src="https://cdn.discordapp.com/attachments/887715476396978176/887720939419213824/blueDark.png" style={{border: none}} alt="blue"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    )
 }
