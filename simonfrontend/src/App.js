80ce999aa24b323df1aee394273691c1d8c5a77f
munibk22 <Munib22@gmail.com>
2021-09-15 18:19:05 -0400

    game board

--- simonfrontend/src/App.js
+++ simonfrontend/src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Game } from './components/Game';
 import './App.css';
+import { Game } from './components/Game';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Game/>
+
+        <Game />
       </header>
     </div>
   );
--- simonfrontend/src/components/game.tsx
+++ simonfrontend/src/components/game.tsx
@@ -1,43 +1,59 @@
 import React, { useState } from "react"
-import '../css/Game.css'
+import '../css/game.css'
 import greenDark from '../img/greenDark.png'
 import redDark from '../img/redDark.png'
 import yellowDark from '../img/yellowDark.png'
 import blueDark from '../img/blueDark.png'
 
 
-export const Game = (sequenceLength:number) => {
+export const Game = (sequenceLength: number) => {
     const [sequence, setSequence] = useState("");
 
     let newSequence = "";
     let times = 0;
     while (++times < sequenceLength) {
-        newSequence += Math.trunc(Math.random()*100) % 4;
+        newSequence += Math.trunc(Math.random() * 100) % 4;
     }
 
     return (<div>
-                <div className="wholegame" style={{paddingTop: "25px", paddingBottom: "20px"}}>
-                    <div className="circle" >
-                        <div className="container" style={{paddingTop: "25px"}}>
-                            <div className="row justify-content-center">
-                                <div className="col aligncenter" >
-                                    <img src={greenDark} style={{border: 'none'}} alt="green"/>
-                                </div>
-                                <div className="col">
-                                    <img src= {redDark} style={{border: 'none'}} alt="red"/>
-                                </div>
-                            </div>
-                            <div className="row justify-content-center" style={{marginTop: "20px"}}>
-                                <div className="col aligncenter" style={{marginLeft: "0px"}}>
-                                    <img src={yellowDark} style={{border: 'none'}} alt="yello"/>
-                                </div>
-                                <div className="col">
-                                    <img src={blueDark} style={{border: 'none'}} alt="blue"/>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+        <div className="wholegame" style={{ paddingTop: "25px", paddingBottom: "20px" }}>
+
+            <div className="" style={{ position: "relative", display: "flex", justifyContent: "center", width: "100vw" }}>
+                <div className="circle" >  </div>
+                <div className="circle2" >  </div>
+                <div className="simoncircle">
+                    <table>
+                        <thead>
+                            <tr>
+
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <img src={greenDark} style={{ border: 'none' }} alt="green" />
+                                </td>
+                                <td>
+                                    <img src={redDark} style={{ border: 'none' }} alt="red" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <img src={yellowDark} style={{ border: 'none' }} alt="yello" />
+                                </td>
+                                <td>
+                                    <img src={blueDark} style={{ border: 'none' }} alt="blue" />
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
+
             </div>
+
+
+
+        </div>
+    </div>
     )
 }
