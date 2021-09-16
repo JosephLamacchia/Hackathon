7bb0a2e4124852f28ba6b227541dd75031bbf2c2
nattee <nsetobol@yahoo.com>
2021-09-15 18:13:14 -0700

    Merge 1 with munbi

--- simonfrontend/src/App.js
+++ simonfrontend/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import { Game } from './components/Game'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Game/>
+        <Game />
       </header>
 
     </div>
